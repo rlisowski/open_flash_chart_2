94fa2e96a9f75c4bc668f964d17ef9859e8b91e4
korin <lisukorin@gmail.com>
2008-07-15 10:28:57 +0200

    froze json, change install description

--- app/controllers/chart_examples_controller.rb
+++ app/controllers/chart_examples_controller.rb
@@ -1,15 +1,20 @@
 class ChartExamplesController < ApplicationController
   before_filter :render_chart
   def render_chart
-    @graph = "<div>
-      #{ofc2(650,300, "charts_ofc2/#{action_name}") unless action_name.eql?('index')}
-    </div>
-    <div>
-      <a href='/charts_ofc2/#{action_name}'>view data</a>
-    </div>
+    unless action_name.eql?('index')
+      @graph = "<div>
+          #{ofc2(650,300, "charts_ofc2/#{action_name}")}
+        </div>
+        <div>
+          <a href='/charts_ofc2/#{action_name}'>view data</a>
+        </div>
       "
       @name = action_name.gsub('_',' ')
       render :action => "show"
+    end
+  end
+  def index
+    
   end
 end
 
